68e479b86ef6fc8ea31c36321aa886ea5f3e196e
Jay <jayridge@gmail.com>
2013-02-25 09:44:43 -0500

    more bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
+###----------------------
+# Simple bootstrap script
 
+REPO=photobaby
+OWNER=jayridge
+
+###----------------------
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get install -y git 
+sudo apt-get install -y git
 
 SRC_DIR=/usr/local/src
-mkdir -p $SRC_DIR
-cd $SRC_DIR
-git clone https://github.com/jayridge/photobaby.git
+GIT_DIR=/usr/local/src/${REPO}
+
+if [ ! -d $SRC_DIR ]; then
+    sudo mkdir -p -m 0777 $SRC_DIR
+    sudo chmod 0777 $SRC_DIR
+fi
+
+if [ ! -d $GIT_DIR ]; then
+    cd $SRC_DIR
+    git clone https://github.com/${OWNER}/${REPO}.git
+fi
+
+sh $GIT_DIR/setup/new_computer.sh
+
---
+++ setup/new_computer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sh apt_tools.sh
+python setup_ssh.py
